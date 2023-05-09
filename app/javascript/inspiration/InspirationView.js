5679e2a1193be8556c9bce497c7a82e2a5ea2dfb
0x534e92 <0x534e92@gmail.com>
2023-05-09 15:51:40 -0500

    add shuffling

--- app/javascript/inspiration/InspirationView.js
+++ app/javascript/inspiration/InspirationView.js
@@ -5,13 +5,12 @@ import { StarIcon as StarIconSolid } from "@heroicons/react/24/solid";
 import { getData } from "inspiration/inspirationSlice";
 import { useDispatch, useSelector } from "react-redux";
 import { Spin } from "antd";
-import { isEmpty } from "lodash";
+import { isEmpty, shuffle } from "lodash";
 import InspirationCard from "inspiration/InspirationCard";
 
 const InspirationView = () => {
   const dispatch = useDispatch();
   const [activeMenu, setActiveMenu] = useState("discover");
-  const [hovered, setHovered] = useState(false);
 
   const fetchingInitialData = useSelector(
     (state) => state.inspiration.fetchingInitialData
@@ -27,9 +26,11 @@ const InspirationView = () => {
 
   const quotes = useMemo(() => {
     if (!isEmpty(standardQuotes)) {
-      return Object.entries(standardQuotes).map(([quoteId, quoteData]) => {
-        return quoteData;
-      });
+      return shuffle(
+        Object.entries(standardQuotes).map(([quoteId, quoteData]) => {
+          return quoteData;
+        })
+      );
     }
 
     return [];
@@ -38,8 +39,6 @@ const InspirationView = () => {
   if (fetchingInitialData) {
     return <Spin />;
   }
-
-  console.log("quotes", quotes);
 
   return (
     <div>
@@ -67,37 +66,6 @@ const InspirationView = () => {
             <InspirationCard quote={quote} key={`discover-quote-${quote.id}`} />
           );
         })}
-        {/*<div className="inspiration-card"></div>*/}
-        {/*<div className="inspiration-card-only-text-black">*/}
-        {/*  <div className="quote-content-container">*/}
-        {/*    <p>*/}
-        {/*      If they can get you asking the wrong questions, they don't have to*/}
-        {/*      worry about answers.*/}
-        {/*    </p>*/}
-        {/*    <p className="quote-author">*/}
-        {/*      <i>Albert Einstein</i>*/}
-        {/*    </p>*/}
-        {/*  </div>*/}
-        {/*</div>*/}
-        {/*<div className="inspiration-card">*/}
-        {/*  <div className="inspiration-card-hover-container">*/}
-        {/*    <StarIconSolid />*/}
-        {/*  </div>*/}
-        {/*</div>*/}
-        {/*<div*/}
-        {/*  className="inspiration-card-only-text-black"*/}
-        {/*  onMouseEnter={() => setHovered(true)}*/}
-        {/*  onMouseLeave={() => setHovered(false)}*/}
-        {/*>*/}
-        {/*  {hovered && (*/}
-        {/*    <div className="inspiration-card-hover-container">*/}
-        {/*      <StarIconSolid />*/}
-        {/*    </div>*/}
-        {/*  )}*/}
-        {/*  You will face many defeats in your life, but never let yourself be*/}
-        {/*  defeated. Face your fear with faith and your fear will become a*/}
-        {/*  fortitude of fiery courage and a future full of promise.*/}
-        {/*</div>*/}
       </div>
     </div>
   );
